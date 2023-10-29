1f01f00d7317b7d7280be14fb8ca5e27ddeeffae
babyshout <lsms0523@gmail.com>
2023-10-29 15:39:43 +0900

    Initial commit

---
+++ src/main/java/com/example/validate_test/ValidateTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.validate_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/validate_test/ValidateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validate_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
